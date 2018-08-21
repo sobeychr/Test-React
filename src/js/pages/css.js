3964462a330149987409b82a6038a524d3ae62e3
Christopher Sobey <christopher.sobey@mindgeek.com>
2018-08-21 17:01:51 -0400

    - Set CSS script

--- src/js/pages/css.js
+++ src/js/pages/css.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import Header from './../header';
+import sortCss from './../scripts/css';
 import './../../scss/pages/css.scss';
 
-const Css = () => (
-    <div>
-        <Header pageName="Css" />
+class Css extends React.Component {
+    constructor(props) {
+        super(props);
 
-        <main className="main">
-            <textarea placeholder="Input Content" className="input" />
-            <pre className="output" />
-        </main>
-    </div>
-);
+        this.state = { text: '' };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        const newText = sortCss(event.target.value);
+        this.setState({ text: newText });
+    }
+
+    render() {
+        return (
+            <div>
+                <Header pageName="Css" />
+
+                <main className="main">
+                    <textarea
+                        placeholder="Input Content"
+                        className="input"
+                        onChange={this.handleChange}
+                    />
+                    <pre className="output">{this.state.text}</pre>
+                </main>
+            </div>
+        );
+    }
+}
 
 export default Css;
---
+++ src/js/scripts/css.js
@@ -0,0 +1,145 @@
+'use strict';
+
+// ================================================================
+// Function sorting the content
+const sortCss = text => {
+    var arr = text.split('\n'),
+        sort = [],
+        isSort = false,
+        compile = [];
+
+    for (let i in arr) {
+        let l = arr[i].toString();
+
+        if (l.indexOf('}') >= 0 || l.indexOf('{') >= 0) {
+            sort.sort(CCssSort.sort);
+            compile = compile.concat(sort);
+            isSort = false;
+            sort = [];
+        }
+
+        if (isSort) {
+            sort.push(new CCssSort(l));
+        } else {
+            compile.push(l);
+        }
+
+        if (l.indexOf('{') >= 0) {
+            isSort = true;
+        }
+    }
+
+    return compile.join('\n');
+};
+
+export default sortCss;
+
+// ================================================================
+// Class managing a single CSS attribute
+class CCssSort {
+    constructor(string) {
+        this.originalstring = string;
+        this.linecut = this.originalstring
+            .replace(/^\s+/, '')
+            .replace(/\s+$/, '');
+
+        this.index = -1;
+        this.property = '';
+        this.propertyAlt = '';
+        this.value = '';
+
+        if (this.linecut.indexOf(':') >= 0) {
+            this.property = /^[^\:\ ]+/.exec(this.linecut)[0];
+            this.propertyAlt = this.property;
+            this.value = /[^\:]+$/.exec(this.linecut)[0];
+
+            if (this.property.indexOf('-') >= 0) {
+                this.propertyAlt = /^[^\-]+/.exec(this.property)[0];
+            }
+
+            var newIndex = CCssSort.order.indexOf(this.property);
+            if (newIndex === -1) {
+                newIndex = CCssSort.order.indexOf(this.propertyAlt);
+            }
+            this.index = newIndex;
+        }
+    }
+
+    toString() {
+        return this.originalstring;
+    }
+
+    // Sorts an array - between 2 CCssSort entries
+    static sort(a, b) {
+        var indexa = a.index,
+            indexb = b.index;
+
+        /*
+        console.log(indexa, a);
+        console.log(indexb, b);
+        console.log('-----');
+        */
+
+        // An element is a space - skip sorting
+        if (a.lineCut === '') {
+            return 1;
+        } else if (b.lineCut === '') {
+            return -1;
+        }
+
+        // Both elements not in the sorting array - alphabetical order
+        else if (indexa === -1 && indexb === -1) {
+            return a.lineCut.localeCompare(b.lineCut);
+        }
+
+        // First element is not in the sorting array - second element is before
+        else if (indexa === -1) {
+            return 1;
+        }
+        // Second element is not in the sorting array - first element is before
+        else if (indexb === -1) {
+            return -1;
+        }
+
+        // Both elements have the same property - alphabetical order
+        else if (indexa === indexb) {
+            return a.property.localeCompare(b.property);
+        }
+
+        return indexa > indexb ? 1 : -1;
+    }
+}
+
+// ================================================================
+// Sorting order
+CCssSort.order = [
+    'display',
+    'flex-wrap',
+    'flex-direction',
+    'flex',
+    'order',
+    'justify-content',
+    'position',
+    'z-index',
+    'top',
+    'bottom',
+    'left',
+    'right',
+    'float',
+    'width',
+    'max-width',
+    'min-width',
+    'height',
+    'max-height',
+    'min-height',
+    'margin',
+    'padding',
+    'border',
+    'background',
+    'font',
+    'color',
+    'text-transform',
+    'text-decoration',
+    'cursor',
+    'content'
+];
