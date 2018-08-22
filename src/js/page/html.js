fbd3adddaf9ba0731fad4fd7b933291d847d4d31
Christopher Sobey <christopher.sobey@mindgeek.com>
2018-08-22 15:01:08 -0400

    + Added HTML script placeholder

--- src/js/page/html.js
+++ src/js/page/html.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import Header from './../component/global/header';
 
-const Html = () => (
-    <div>
-        <Header pageName="Html" />
-        <h1>test</h1>
-    </div>
-);
+class Html extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <Header pageName="Html" />
+                <main className="main">
+                    <h1>Future page to auto-indent HTML text</h1>
+                    <h2>From</h2>
+                    <pre>
+                        &lt;a href="testLink" class="testClass"
+                        data-test="testValue"&gt;test&lt;/a&gt;
+                    </pre>
+                    <h3>To</h3>
+                    <pre>
+                        &lt;a href="testLink"
+                        {'\n   '}
+                        class="testClass"
+                        {'\n   '}
+                        data-test="testValue"&gt;
+                        {'\n   '}
+                        test
+                        {'\n'}
+                        &lt;/a&gt;
+                    </pre>
+                </main>
+            </div>
+        );
+    }
+}
 
 export default Html;
